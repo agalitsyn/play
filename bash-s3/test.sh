8e1baff4d11d64c5917a1998d173a489fdbed3b7
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-10-28 19:31:17 +0700

    more on s3

--- bash-s3/test.sh
+++ bash-s3/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail; [[ $TRACE ]] && set -x
+set -eo pipefail
 
 SCRIPT_DIR="$(dirname "$0")"
 
@@ -9,13 +9,32 @@ main() {
     local bucket=${1:?$usage}
     local object=${2:?$usage}
 
-    source "$SCRIPT_DIR/../../../mietek/bashmenot/src.sh"
+	local temp_file=
 
-    export BASHMENOT_AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+	export BASHMENOT_NO_SELF_UPDATE=1
+	export BASHMENOT_AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
     export BASHMENOT_AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
     export BASHMENOT_S3_ENDPOINT="$AWS_S3_ENDPOINT_URL"
 
-    s3_check "$bucket" "$object"
+    source "$SCRIPT_DIR/../../../mietek/bashmenot/src.sh"
+
+	temp_file="$(mktemp)"
+	echo "Hi there, I was created at $(date -u -R)!" > "$temp_file"
+
+	echo "==> Create temp file and upload it"
+	#set -x
+	s3_upload "$temp_file" "$bucket" "$object" "public-read"
+	#set +x
+
+	echo "==> Check file exists in S3"
+	#set -x
+	s3_check "$bucket" "$object"
+	#set +x
+
+	echo "==> Get file content"
+	#set -x
+	s3_download "$bucket" "$object" /dev/stdout
+	#set +x
 }
 
 main "$@"
