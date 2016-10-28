4698e9dbfd0993f07a92b87ee25e655311972eb2
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-10-28 16:17:07 +0700

    update script

--- bash-s3/test.sh
+++ bash-s3/test.sh
@@ -5,15 +5,17 @@ set -eo pipefail; [[ $TRACE ]] && set -x
 SCRIPT_DIR="$(dirname "$0")"
 
 main() {
-    local usage="$0 <bucket-name>"
+    local usage="$0 <bucket-name> <object-name>"
     local bucket=${1:?$usage}
+    local object=${2:?$usage}
 
     source "$SCRIPT_DIR/../../../mietek/bashmenot/src.sh"
 
     export BASHMENOT_AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
     export BASHMENOT_AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+    export BASHMENOT_S3_ENDPOINT="$AWS_S3_ENDPOINT_URL"
 
-    s3_list "$bucket" ""
+    s3_check "$bucket" "$object"
 }
 
 main "$@"
