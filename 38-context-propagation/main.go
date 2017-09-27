3421930ad369992a5f4584dd67e7013c3e2fd0dc
mr1name <alt13ri@gmail.com>
2017-09-28 00:15:49 +0700

    fixed bugs

--- 38-context-propagation/main.go
+++ 38-context-propagation/main.go
@@ -51,13 +51,19 @@ func gen(ctx context.Context, wg *sync.WaitGroup) <-chan int {
 
 func merge(ctx context.Context, wg *sync.WaitGroup, inputs ...<-chan int) <-chan int {
 	var lwg sync.WaitGroup
-	outCh := make(chan int)
+	outCh := make(chan int, len(inputs))
 
 	receiver := func(ch <-chan int) {
 		defer lwg.Done()
-		for i := range ch {
+		for {
 			select {
-			case outCh <- i:
+			case v := <-ch:
+				select {
+				case <-ctx.Done():
+					log.Printf("close receiver: %v", ctx.Err())
+					return
+				case outCh <- v:
+				}
 			case <-ctx.Done():
 				log.Printf("close receiver: %v", ctx.Err())
 				return
@@ -67,7 +73,9 @@ func merge(ctx context.Context, wg *sync.WaitGroup, inputs ...<-chan int) <-chan
 
 	lwg.Add(len(inputs))
 	for _, input := range inputs {
-		go receiver(input)
+		go func(i <-chan int) {
+			receiver(i)
+		}(input)
 	}
 
 	go func() {
