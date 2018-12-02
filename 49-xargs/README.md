b813e952f2b50c3ca78c23d8a96c140b66ea28b0
Anton Galitsyn <agalitsyn@users.noreply.github.com>
2018-12-02 16:25:42 +0700

    update

--- 49-xargs/README.md
+++ 49-xargs/README.md
@@ -1,9 +1,22 @@
 # xargs
 
-Run `go build` before running examples.
+Run `go run main.go --help` for details. Also you might want to compile it using `go build`.
 
 ## Examples
 
 ```bash
-$ for i in {1..60} ; do echo $i; done | ./49-xargs echo {}
+# classic
+$ for i in {1..60}; do echo $i; done | go run main.go echo {}
+
+# with rate limiting (will took ~4s)
+$ for i in {1..60} ; do echo $i; done | go run main.go -rate 15 echo {}
+
+# with parallel processes (will took ~1s)
+$ for i in {1..60} ; do echo $i; done | go run main.go -rate 15 -inflight 4 echo {}
+
+# will took ~2s
+$ (echo 1; echo 2; echo 3; echo 4) | go run main.go -rate 1 -inflight 2 -debug -timer echo {}
+
+# will took ~4s
+$ (echo 1 ; sleep 3 ; echo 2 ; echo 3) | go run main.go -rate 1 -inflight 2 echo {}
 ```
--- 49-xargs/main.go
+++ 49-xargs/main.go
@@ -2,48 +2,127 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
-func main() {
-	flag.Parse()
+type config struct {
+	Rate       float64
+	RatePeriod time.Duration
+	Inflight   uint
+}
 
+func main() {
+	var cfg config
+	flag.Float64Var(&cfg.Rate, "rate", 0, "Limit of actions per one period for one parallel run.")
+	flag.DurationVar(&cfg.RatePeriod, "rate-period", time.Second, "Period for rate actions.")
+	flag.UintVar(&cfg.Inflight, "inflight", 1, "Limit for parallel runs.")
+
+	var timer = flag.Bool("timer", false, "Run intergated timer")
+	var debug = flag.Bool("debug", false, "Output debug info")
+
+	flag.Parse()
 	if flag.NArg() < 2 {
 		log.Fatal("missing command to execute")
 	}
-	cmdName := flag.Arg(0)
-	cmdArgs := flag.Args()[1:]
 
-	out := recieve(os.Stdin)
-	for b := range out {
-		// no reuse :( https://golang.org/pkg/os/exec/#Cmd
-		cmd := exec.Command(cmdName, cmdArgs...)
-
-		const placeholder = `{}`
-		for i := range cmd.Args {
-			if cmd.Args[i] == placeholder {
-				cmd.Args[i] = string(b)
-				break
-			}
-		}
-
-		// user might want to pass commands like `time echo 1`, which logs to stderr
-		cmdOut, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Fatalf("could not execute command: %v", err)
-		}
-
-		fmt.Print(string(cmdOut))
+	if *debug {
+		log.Printf("started with config: %+v", cfg)
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigquit := make(chan os.Signal, 1)
+	signal.Notify(sigquit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		s := <-sigquit
+		log.Printf("captured %v, exiting...", s)
+		cancel()
+	}()
+
+	if *timer {
+		defer timeTaken(time.Now())
+	}
+
+	cmdName := flag.Arg(0)
+	args := flag.Args()[1:]
+
+	cmdArgs := make([]string, len(args))
+	copy(cmdArgs, args)
+	placeholderIndex, shouldReplace := indexPlaceholder(`{}`, cmdArgs)
+
+	subProcess := func(replace string) {
+		if shouldReplace {
+			cmdArgs[placeholderIndex] = replace
+		}
+
+		if *debug {
+			log.Printf("%s %v", cmdName, cmdArgs)
+		}
+
+		out, err := runCmd(cmdName, cmdArgs...)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Fprint(os.Stdout, out)
+	}
+
+	// no rate limiting, run and exit
+	if cfg.Rate == 0 {
+		for s := range readPipe(ctx, os.Stdin) {
+			subProcess(s)
+		}
+		return
+	}
+
+	// with rate-limiter
+	period := (float64(cfg.RatePeriod) / cfg.Rate) / float64(cfg.Inflight)
+	limiter := time.Tick(time.Duration(period))
+	queue := readPipe(ctx, os.Stdin)
+
+	// spawn workers
+	var wg sync.WaitGroup
+	var i uint
+	for i = 0; i < cfg.Inflight; i++ {
+		wg.Add(1)
+		go func(queue <-chan string, id uint) {
+			defer wg.Done()
+
+			for s := range queue {
+				if *debug {
+					log.Printf("worker %d got %s", id, s)
+				}
+
+				select {
+				case <-ctx.Done():
+					return
+				case <-limiter:
+					subProcess(s)
+				}
+			}
+		}(queue, i)
+	}
+	if *debug {
+		log.Printf("period: %v", period)
+		log.Printf("%v workers", i)
+	}
+
+	wg.Wait()
 }
 
-func recieve(r io.Reader) <-chan []byte {
-	out := make(chan []byte)
+func readPipe(ctx context.Context, r io.Reader) <-chan string {
+	out := make(chan string)
 	go func() {
 		defer close(out)
 
@@ -52,7 +131,11 @@ func recieve(r io.Reader) <-chan []byte {
 			b := make([]byte, len(s.Bytes()))
 			copy(b, s.Bytes())
 
-			out <- b
+			select {
+			case out <- string(b):
+			case <-ctx.Done():
+				return
+			}
 		}
 
 		if err := s.Err(); err != nil {
@@ -60,4 +143,26 @@ func recieve(r io.Reader) <-chan []byte {
 		}
 	}()
 	return out
+}
+
+func runCmd(name string, arg ...string) (string, error) {
+	cmd := exec.Command(name, arg...)
+	cmdOut, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("could not execute command: %v", err)
+	}
+	return string(cmdOut), nil
+}
+
+func indexPlaceholder(placeholder string, args []string) (int, bool) {
+	for i := range args {
+		if args[i] == placeholder {
+			return i, true
+		}
+	}
+	return 0, false
+}
+
+func timeTaken(t time.Time) {
+	log.Printf("took %s\n", time.Since(t))
 }
