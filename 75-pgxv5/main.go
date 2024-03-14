b4cbbcc4fab169883eee6fddbca4ccac1cfcd264
Anton Galitsyn <anton.galitsyn@gmail.com>
2024-03-14 19:23:06 +0700

    play

---
+++ 75-pgxv5/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
+	var greeting string
+	err = conn.QueryRow(ctx, "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
+}
